1771707a600adb4ba9531d3466ea787954ec43a8
Булгаков Михаил Сергеевич <m.bulgakov@main.nsu.ru>
2024-10-28 10:09:06 +0700

    В понедельник

--- task7.py
+++ task7.py
@@ -5,7 +5,10 @@
 # [20, -90, 15, -34, 10, 0]
 # Вывод: [20, 15, 10, -90, -34, 0]
 
+'''
 import random
+from colored import fg
+
 N = int(input())
 m = []
 for j in range(N):
@@ -13,4 +16,24 @@ for j in range(N):
     for i in range (N):
         k = random.randint(32, 126)
         m.append(chr(k))
-        print(chr(k), end="")
+        color = fg('green')
+        print(color + chr(k), end="")
+'''
+
+import random
+N = int(input())
+m = []
+j = []
+f = 0
+for i in range (N):
+    k = random.randint(0, 100)
+    z = random.randint(0, 1)
+    if z == 0:
+        l = k*(-1)
+        k=l
+    if k>0:
+        m.append(k)
+    else:
+        j.append(k)
+
+print(m+j)
--- task8.py
+++ task8.py
@@ -4,3 +4,20 @@
 # Пример: ввод N = 4
 # [12, 77, 5555, 97]
 # Вывод: [77, 5555]
+
+import random
+N = int(input())
+m = []
+f = 0
+for i in range (N):
+    k = random.randint(10, 100000)
+    if (k%11 == 0 and k < 100) :
+        m.append(k)
+    elif (k%111 ==0 and k>100 and k<1000):
+        m.append(k)
+    elif (k%1111 == 0 and k>1000 and k < 10000):
+        m.append(k)
+    elif k%11111==0:
+        m.append(k)
+
+print(m)
